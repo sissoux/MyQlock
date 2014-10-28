e08f6f8780ed718f8d713eb40e5840821ad46488
benjamin descamps <benjamin.descamps@cgi.com>
2014-10-28 10:00:23 +0100

    cleanup

--- My_Qlock.ino
+++ My_Qlock.ino
@@ -3,7 +3,7 @@
 #include "define.h"
 #include <CapacitiveSensor.h>
 
-
+//TODO Faire une distinction clic long, clic rapide.
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(110, 6, NEO_GRB + NEO_KHZ800);    // Declare the LED strip using Adafruit library
 
@@ -11,12 +11,13 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(110, 6, NEO_GRB + NEO_KHZ800);    //
 CapacitiveSensor   RcapSensor = CapacitiveSensor(22,18);        // Right capacitive sensor
 CapacitiveSensor   LcapSensor = CapacitiveSensor(22,19);        // Left capacitive sensor
 int TouchCounter = 0;
+int screenRefreshRate = 20;
 
 byte FlagRegister = 0;
 byte Blink = 0;
 
 boolean Error = false;            // Error state
-boolean RefreshFlag = false;      // Used to tell if strip has been refreshed
+//boolean RefreshFlag = false;      // Used to tell if strip has been refreshed
 
 uint32_t OutBuffer_0[LED_COUNT];    // First buffer of 2 for circular buffering. Contains Pixel color
 uint32_t OutBuffer_1[LED_COUNT];    // Second buffer of 2 for circular buffering. Contains Pixel color
@@ -24,8 +25,8 @@ int ActiveBuffer = 0;               // Contains the active (to be displayed) buf
 
 elapsedMillis RefreshTimer;          // auto incremented value, called to sequence and synchronize tasks ==> Output refresh rate
 elapsedMillis ColorTimer;        
-elapsedMillis InputRefresh;     
-elapsedMillis Blinker;          
+elapsedMillis InputRefreshTimer;     
+elapsedMillis BlinkerTimer;          
 
 uint16_t h = 1;                      // Temp var used to store current Hour
 uint16_t m = 0;                      // Temp var used to store current Minute
@@ -34,6 +35,8 @@ uint32_t ColorMask;
 rgb MyRGBColor;
 hsv MyHSVColor;
 int SatSign = -1;
+
+boolean hasToBeReDrawnNow = false;
 
 enum colormode{
   FADING,
@@ -82,142 +85,150 @@ void setup()  {
   MyHSVColor.h = 150.0;
   MyHSVColor.s = 1.0;
   MyHSVColor.v = 1.0;
+  
+	if (ColorMode == STATIC)
+	{
+		//We use a slow refresh rate
+		screenRefreshRate = 500;
+	}
+	else
+	{
+		//We can use a fast refresh rate
+		screenRefreshRate = REFRESH_RATE;
+	}
 }
 
-void loop() {
-
-  //// Display refresh check section ////
-  if (RefreshTimer >= REFRESH_RATE) 
-  {
-    RefreshTimer = 0;//RefreshTimer - REFRESH_RATE;
-
-    writeTime();
-
-    if ( CurrentState != STBY)
-    {
-      refreshOutput();
-      RefreshFlag = 1;
-    }
-
-  }
-
-  //// Display refresh check section ////
-  if (ColorTimer >= COLOR_CHANGE_RATE) 
-  {
-    ColorTimer = 0;//ColorTimer - COLOR_CHANGE_RATE;
-    colorMaskUpdate();
-
-  }
-
-  if (InputRefresh >= INPUT_REFRESH_RATE) 
-  {
-    InputRefresh = 0;
-    sensorCheck();
-    navig();
-  }
-
-  if (Blinker >= BLINK_RATE) 
-  {
-    Blinker = 0;
-    Blink = !Blink;
-  }
-
-
+void loop()
+{
+	if (CurrentState != STBY)
+	{
+		//Here, you'll find everything related to graphics because it matters only when != STBY
+		if (RefreshTimer >= screenRefreshRate || hasToBeReDrawnNow) 
+		{
+			RefreshTimer = 0;
+			writeTime();
+			refreshOutput();
+			hasToBeReDrawnNow = false;
+			//RefreshFlag = true;
+		}
+		if (ColorTimer >= COLOR_CHANGE_RATE) 
+		{
+			ColorTimer = 0;
+			//Performance : Do not fill stack
+			if (ColorMode == FADING)
+			{
+				colorMaskUpdate();
+				hasToBeReDrawnNow = true;
+			}
+		}
+		if (BlinkerTimer >= BLINK_RATE) 
+		{
+			BlinkerTimer = 0;
+			Blink = !Blink;
+			hasToBeReDrawnNow = true;
+		}
+	}
+	
+	if (InputRefreshTimer >= INPUT_REFRESH_RATE) 
+	{
+		InputRefreshTimer = 0;
+		sensorCheck();
+		navig();
+	}
 }
 
+void delay2(int ms)
+{
+	unsigned long Now = millis();
+	while (millis() - Now < ms);
+}
 
 void navig()
 {
-  switch (CurrentState)
-  {
-  case NORMAL:
-    if( (FlagRegister & (R_FLAG_MASK | L_FLAG_MASK)) == (R_FLAG_MASK | L_FLAG_MASK))
-    {
-      CurrentState = HSET;
-      writeFrame(1);
-      unsigned long Now = millis();
-      while (millis() - Now < 1500);
-    }
-    else if (FlagRegister & L_FLAG_MASK)
-    {
-      if ( TouchCounter >= 5)
-      {
-        TouchCounter == 0;
-      CurrentState = STBY;
-      writeFrame(BYE_FRAME);
-      unsigned long Now = millis();
-      while (millis() - Now < 1500);
-      }
-      else 
-      {
-        TouchCounter++;
-      }
-    }
-    else
-    {
-      TouchCounter = 0;
-    }
-    break;
+	switch (CurrentState)
+	{
+		case NORMAL:
+		if(FlagRegister & (R_FLAG_MASK | L_FLAG_MASK))
+		{
+			CurrentState = HSET;
+			writeFrame(H_FRAME);
+			delay2(1500);
+		}
+		else if (FlagRegister & L_FLAG_MASK)
+		{
+			if (TouchCounter >= 5)
+			{
+				CurrentState = STBY;
+				writeFrame(BYE_FRAME);
+				delay2(1500);
+			}
+			else 
+			{
+				TouchCounter++;
+			}
+		}
+		else
+		{
+		  TouchCounter = 0;
+		}
+		break;
+		
+		case HSET:
+		if (FlagRegister & L_FLAG_MASK)
+		{
+			CurrentState = MSET;
+			writeFrame(M_FRAME);
+			delay2(1500);
+		}
+		if (FlagRegister & R_FLAG_MASK)
+		{
+			Teensy3Clock.set((time_t)(now()+3600));
+			setTime(hour()+1,minute(),second(),day(),month(),year());
+		}
+		break;
 
-  case HSET:
-    if (FlagRegister & L_FLAG_MASK)
-    {
-      CurrentState = MSET;
-      writeFrame(2);
-      unsigned long Now = millis();
-      while (millis() - Now < 1500);
-    }
-    if (FlagRegister & R_FLAG_MASK)
-    {
-      Teensy3Clock.set((time_t)(now()+3600));
-      setTime(hour()+1,minute(),second(),day(),month(),year());
-    }
-    break;
+		case MSET:
+		if (FlagRegister & L_FLAG_MASK)
+		{
+			CurrentState = NORMAL;
+		}
+		if (FlagRegister & R_FLAG_MASK)
+		{
+			Teensy3Clock.set((time_t)(now()+300));
+			setTime(hour(),minute()+5,second(),day(),month(),year());
+		}    
+		break;
 
-  case MSET:
-    if (FlagRegister & L_FLAG_MASK)
-    {
-      CurrentState = NORMAL;
-    }
-    if (FlagRegister & R_FLAG_MASK)
-    {
-      Teensy3Clock.set((time_t)(now()+300));
-      setTime(hour(),minute()+5,second(),day(),month(),year());
-    }    
-    break;
+		case LUMSET:
+		if (FlagRegister & L_FLAG_MASK)
+		{
+			CurrentState = NORMAL;
+		}
+		break;
 
-  case LUMSET:
-    if (FlagRegister & L_FLAG_MASK)
-    {
-      CurrentState = NORMAL;
-    }
-    break;
+		case LUMSENS:
+		if (FlagRegister & L_FLAG_MASK)
+		{
+			CurrentState = NORMAL;
+		}
+		break;  
 
-  case LUMSENS:
-    if (FlagRegister & L_FLAG_MASK)
-    {
-      CurrentState = NORMAL;
-    }
-    break;  
-
-  case STBY:
-    if (FlagRegister & L_FLAG_MASK)
-    {
-      CurrentState = NORMAL;
-      writeFrame(HI_FRAME);
-      unsigned long Now = millis();
-      while (millis() - Now < 1500);
-    }
-    break;    
-  }
-  FlagRegister &= ~(R_FLAG_MASK | L_FLAG_MASK);
+		case STBY:
+		if (FlagRegister & L_FLAG_MASK)
+		{
+			CurrentState = NORMAL;
+			writeFrame(HI_FRAME);
+			delay2(1500);
+		}
+		break;    
+	}
+	FlagRegister &= ~(R_FLAG_MASK | L_FLAG_MASK);
 }
 
 void colorMaskUpdate()
 {
   if (ColorMode == FADING)
   {
-
     MyHSVColor.h += 0.02;
     if (MyHSVColor.h>360) MyHSVColor.h = 0;
 
@@ -237,47 +248,34 @@ void colorMaskUpdate()
 void sensorCheck()
 {
   long LcapSensorVal =  LcapSensor.capacitiveSensor(30);      //Launch capacitive sensor acquisition, return corresponding delay
-  unsigned long Now = millis();
-  while (millis() - Now < 50);                                                          // Delay ==> Ease the to enter configuration
+  delay2(50);
   long RcapSensorVal =  RcapSensor.capacitiveSensor(30);
 
   if(RcapSensorVal > R_SENSOR_THRESHOLD)      // If returned value is greater than Threshold, debounce and raise corresponding FLAG
   {
-    //while (RcapSensorVal > R_SENSOR_THRESHOLD)  RcapSensorVal =  RcapSensor.capacitiveSensor(30);     // Wait until touch is released
-    unsigned long Now = millis();
-    while (millis() - Now < DEBOUNCE_DELAY);                                                          // Wait DEBOUNCE DELAY more to avoid multi detections
+	delay2(200);
     FlagRegister |= R_FLAG_MASK;
   }
 
   if(LcapSensorVal > L_SENSOR_THRESHOLD)
   {
-    //while (LcapSensorVal > L_SENSOR_THRESHOLD)  LcapSensorVal =  LcapSensor.capacitiveSensor(30);
-    unsigned long Now = millis();
-    while (millis() - Now < DEBOUNCE_DELAY);
+	delay2(200);
     FlagRegister |= L_FLAG_MASK;
   }
 }
 
 void refreshOutput()
 {
-  uint32_t TempColor;             // For debug purpose
+  //uint32_t TempColor;             // For debug purpose
+  
+	uint32_t OutBuffer = ActiveBuffer ? OutBuffer_1 : OutBuffer_0;
 
-  if (!ActiveBuffer)                              // If active buffer = 0 then use OutBuffer_0
-  {
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, OutBuffer_0[Mapping[i]]);
-    }
-  }
-  else
-  {
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, OutBuffer_1[Mapping[i]]); 
-    }
-  }
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, OutBuffer[Mapping[i]]); 
+	}
 
-  strip.show();                // Display selected buffer
+	strip.show();                // Display selected buffer
 }
 
 time_t getTeensy3Time()      // Function used by RTC
@@ -285,44 +283,22 @@ time_t getTeensy3Time()      // Function used by RTC
   return Teensy3Clock.get();
 }
 
-
 void writeTime()
 {
-  m = minute();
-  h = hour();
-  if ( m >=35)
-  {
-    h++;
-  }
-  h = h % 12;
-  if (h == 0)
-  {
-    if (isPM())
-    {
-      if (m>=35)
-      {
-        h = 13;
-      }
-      else
-      {
-        h = 12;
-      }
-    }
-    else
-    {
-      if (m>=35)
-      {
-        h = 12;
-      }
-      else
-      {
-        h = 13;
-      }
-    }
-  }
+	m = minute();
+	h = hour();
+	if (m >=35)
+	{
+		h++;
+	}
+	h = h%12;
+	if (h == 0)
+	{
+		h = isPM() ? 13 : 12;
+	}
 
-  int FithOfMin = m / 5;
-  clear_Buffer(ActiveBuffer);
+	int FithOfMin = m / 5;
+	clear_Buffer(ActiveBuffer);
 
   for (int pixel = 0 ; pixel < 66 ; pixel++)            // Fill in HOUR pixels
   {
@@ -352,57 +328,28 @@ void writeTime()
       } 
     }  
   }
-
-
 }
 
 void fill_Buffer(int BufferID, int PixelID, uint32_t Color)
 {
-  if (!BufferID)
-  {
-    OutBuffer_0[PixelID] = Color;
-  }
-  else
-  {
-    OutBuffer_1[PixelID] = Color;
-  }
+	uint32_t OutBuffer = BufferID ? OutBuffer_1 : OutBuffer_0;
+	OutBuffer[PixelID] = Color;
 }
 
 uint32_t get_Buffer(int BufferID, int PixelID)
 {
-  if (!BufferID)
-  {
-    return OutBuffer_0[PixelID];
-  }
-  else
-  {
-    return OutBuffer_1[PixelID];
-  }
+	uint32_t OutBuffer = BufferID ? OutBuffer_1 : OutBuffer_0;
+	return OutBuffer[PixelID];
 }
 
 void clear_Buffer(int BufferID)
 {
-  if (!BufferID)
-  {
-    for (int Pixel = 0; Pixel<110; Pixel++)
-    {
-      OutBuffer_0[Pixel] = 0;
-    }
-  }
-  else
-  {
-    for (int Pixel = 0; Pixel<110; Pixel++)
-    {
-      OutBuffer_1[Pixel] = 0;
-    }
-  }
+	uint32_t OutBuffer = BufferID ? OutBuffer_1 : OutBuffer_0;
+	for (int Pixel = 0; Pixel<110; Pixel++)
+	{
+		OutBuffer[Pixel] = 0;
+	}
 }
-
-
-
-
-
-
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
@@ -420,14 +367,12 @@ uint32_t Wheel(byte WheelPos) {
   }
 }
 
-
-
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
-    delay(wait);
+    delay2(wait);
   }
 }
 
@@ -439,139 +384,131 @@ void rainbow(uint8_t wait) {
       strip.setPixelColor(i, Wheel((i+j) & 255));
     }
     strip.show();
-    delay(wait);
+    delay2(wait);
   }
 }
-
 
 void writeFrame(int FrameID)
 {
   uint8_t x, y, r, g, b, pixel, brightness;
   switch (FrameID)
   {
-  case 0:
+	case 0:
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, 0);
+	}
+	break;
 
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
+	case H_FRAME:
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, 0);
+	}
 
-    break;
+	for (int i = 0; i<(sizeof(FrameH)/5);i++)
+	{
+		x = FrameH[i][X];
+		y = FrameH[i][Y];
+		r = FrameH[i][R];
+		g = FrameH[i][G];
+		b = FrameH[i][B];
+		if (y%2)
+		{
+			pixel = x+11*y;
+		}
+		else
+		{
+			pixel = 10-x+11*y;
+		}
+		strip.setPixelColor(pixel, strip.Color(r, g, b));
+	}
+	break;
 
-  case H_FRAME:
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
+	case M_FRAME:
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, 0);
+	}
 
-    for (int i = 0; i<(sizeof(FrameH)/5);i++)
-    {
-      x = FrameH[i][X];
-      y = FrameH[i][Y];
-      r = FrameH[i][R];
-      g = FrameH[i][G];
-      b = FrameH[i][B];
-      if (y%2)
-      {
-        pixel = x+11*y;
-      }
-      else
-      {
-        pixel = 10-x+11*y;
-      }
-      strip.setPixelColor(pixel, strip.Color(r, g, b));
-    }
-    break;
+	for (int i = 0; i<(sizeof(FrameM)/5);i++)
+	{
+		x = FrameM[i][X];
+		y = 9-FrameM[i][Y];
+		r = FrameM[i][R];
+		g = FrameM[i][G];
+		b = FrameM[i][B];
+		if (y%2)
+		{
+			pixel = x+11*y;
+		}
+		else
+		{
+			pixel = 10-x+11*y;
+		}
+		strip.setPixelColor(pixel, strip.Color(r, g, b));
+	}
+	break;
 
-  case M_FRAME:
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
+	case HI_FRAME:
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, 0);
+	}
 
-    for (int i = 0; i<(sizeof(FrameM)/5);i++)
-    {
-      x = FrameM[i][X];
-      y = 9-FrameM[i][Y];
-      r = FrameM[i][R];
-      g = FrameM[i][G];
-      b = FrameM[i][B];
-      if (y%2)
-      {
-        pixel = x+11*y;
-      }
-      else
-      {
-        pixel = 10-x+11*y;
-      }
-      strip.setPixelColor(pixel, strip.Color(r, g, b));
-    }
-    break;
+	for (brightness = 0 ; brightness < 255 ; brightness++)
+	{ 
+		for (int i = 0; i<(sizeof(FrameHI)/5) ; i++)
+		{
+			x = 10-FrameHI[i][X];
+			y = 9-FrameHI[i][Y];
+			r = FrameHI[i][R];
+			g = FrameHI[i][G];
+			b = FrameHI[i][B];
+			if (y%2)
+			{
+				pixel = x+11*y;
+			}
+			else
+			{
+				pixel = 10-x+11*y;
+			}
+			strip.setPixelColor(pixel, strip.Color(brightness, brightness, brightness));
+		}
+		strip.show();
+		delay2(20);
+	}
+	break;
 
-  case HI_FRAME:
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
+	case BYE_FRAME:
+	for(uint16_t i=0; i<strip.numPixels(); i++) 
+	{
+		strip.setPixelColor(i, 0);
+	}
 
-    for (brightness = 0 ; brightness < 255 ; brightness++)
-    { 
-      for (int i = 0; i<(sizeof(FrameHI)/5) ; i++)
-      {
-        x = 10-FrameHI[i][X];
-        y = 9-FrameHI[i][Y];
-        r = FrameHI[i][R];
-        g = FrameHI[i][G];
-        b = FrameHI[i][B];
-        if (y%2)
-        {
-          pixel = x+11*y;
-        }
-        else
-        {
-          pixel = 10-x+11*y;
-        }
-        strip.setPixelColor(pixel, strip.Color(brightness, brightness, brightness));
-      }
-      strip.show();
-      delay(20);
-    }
-    break;
-
-  case BYE_FRAME:
-    for(uint16_t i=0; i<strip.numPixels(); i++) 
-    {
-      strip.setPixelColor(i, 0);
-    }
-
-    for (brightness = 255 ; brightness > 0 ; brightness--)
-    { 
-      for (int i = 0; i<(sizeof(FrameBYE)/5);i++)
-      {
-        x = 10-FrameBYE[i][X];
-        y = 9-FrameBYE[i][Y];
-        r = FrameBYE[i][R];
-        g = FrameBYE[i][G];
-        b = FrameBYE[i][B];
-        if (y%2)
-        {
-          pixel = x+11*y;
-        }
-        else
-        {
-          pixel = 10-x+11*y;
-        }
-        strip.setPixelColor(pixel, strip.Color(brightness, brightness, brightness));
-      }
-      strip.show();
-      delay(20);
-    }
-    break;
+	for (brightness = 255 ; brightness > 0 ; brightness--)
+	{ 
+		for (int i = 0; i<(sizeof(FrameBYE)/5);i++)
+		{
+			x = 10-FrameBYE[i][X];
+			y = 9-FrameBYE[i][Y];
+			r = FrameBYE[i][R];
+			g = FrameBYE[i][G];
+			b = FrameBYE[i][B];
+			if (y%2)
+			{
+				pixel = x+11*y;
+			}
+			else
+			{
+				pixel = 10-x+11*y;
+			}
+			strip.setPixelColor(pixel, strip.Color(brightness, brightness, brightness));
+		}
+		strip.show();
+		delay2(20);
+	}
+	break;
   }
-
   strip.show();
 }
-
-
-
-
